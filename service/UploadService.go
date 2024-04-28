fa9d2943996433f809094d61c9f95481350b2a02
johannes-kuhfuss <johannes.kuhfuss@web.de>
2024-04-28 08:23:00 +0200

    WiP

--- service/UploadService.go
+++ service/UploadService.go
@@ -1,9 +1,11 @@
 package service
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path"
+	"time"
 
 	"github.com/johannes-kuhfuss/fileupload/config"
 	"github.com/johannes-kuhfuss/fileupload/dto"
@@ -39,6 +41,25 @@ func (s DefaultUploadService) Upload(fd dto.FileDta) (written int64, err error) 
 }
 
 func buildFileName(uploadPath string, bcDate string, startTime string, endTime string, fileName string) string {
-	logger.Info(bcDate + startTime + endTime)
-	return path.Join(uploadPath, fileName)
+	bcd, err := time.Parse("2006-01-02", bcDate)
+	if err != nil {
+		logger.Error("Could not parse date.", err)
+	}
+	yStr := fmt.Sprintf("%02d", bcd.Year())
+	mStr := fmt.Sprintf("%02d", bcd.Month())
+	dStr := fmt.Sprintf("%02d", bcd.Day())
+	st, err := time.Parse("15:04", startTime)
+	if err != nil {
+		logger.Error("Could not start time.", err)
+	}
+	et, err := time.Parse("15:04", endTime)
+	if err != nil {
+		logger.Error("Could not end time.", err)
+	}
+	stStr := fmt.Sprintf("%02d%02d", st.Hour(), st.Minute())
+	etStr := fmt.Sprintf("%02d%02d", et.Hour(), et.Minute())
+	fn := "UL__" + stStr + "-" + etStr + "__" + fileName
+
+	os.MkdirAll(path.Join(uploadPath, yStr, mStr, dStr), os.ModePerm)
+	return path.Join(uploadPath, yStr, mStr, dStr, fn)
 }
